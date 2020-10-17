b824dba3c6dedae2161b1b02b4a73aa4db3c6722
hkm007 <hkm0072001@gmail.com>
2020-10-17 13:30:09 +0530

    material UI ditched

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,12 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import './css/navbar.css';
 
 function Navbar() {
-    const classes = useStyles();
-
     return (
-        <div className={classes.root}>
-            <AppBar position="static" style={{ background: '#000000' }}>
-                <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                    BitFields
-                </Typography>
-                <Button onClick={() => alert('Login')} color="primary" variant="contained" style={{marginRight: '5px'}}>Login</Button>
-                <Button onClick={() => alert('SignUp')} color="secondary" variant="contained">Signup</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <React.Fragment>
+            <h3 className={'title'}>Hello Navbar</h3>
+        </React.Fragment>
     )
-}
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    }
-}));
-  
+} 
 
 export default Navbar;
