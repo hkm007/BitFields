128145535a6f2e5559f4ea6286da3b07d9f0294b
hkm007 <hkm0072001@gmail.com>
2020-10-17 16:11:22 +0530

    nav added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,14 @@ import './css/navbar.css';
 function Navbar() {
     return (
         <React.Fragment>
-            <h3 className={'title'}>Hello Navbar</h3>
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="#">BitFields</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
         </React.Fragment>
     )
 } 
